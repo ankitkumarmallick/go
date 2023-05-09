0e360bf6a0e45f914e75ebb22b6b026d165f8613
Ankit Kumar <amallick96@outlook.com>
2023-05-09 19:42:47 -0400

    added function to send http get requests

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
 	args := os.Args
-	fmt.Printf("Hello world\nArgument: %v", args[1:])
+	if len(args) < 2 {
+		fmt.Printf("Usage of this function is: ./http-get <URL>\n")
+		os.Exit(1)
+	}
+	uri := args[1]
+	if _, error := url.ParseRequestURI(uri); error != nil {
+		fmt.Printf("Invalid URL: %s\n", error)
+		os.Exit(1)
+	}
+	response, http_err := http.Get(uri)
+	if http_err != nil {
+		fmt.Printf("Error: %s\n", http_err)
+		os.Exit(1)
+	}
+	fmt.Printf("Response: %v\n", response)
+
 }
